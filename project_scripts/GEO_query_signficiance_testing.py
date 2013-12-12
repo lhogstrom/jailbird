eddb3e4db12eb1c71ac88eb2466a320fb7736acd
Larson Hogstrom <hogstrom@broadinstitute.org>
2013-12-12 18:36:40 -0500

    picking out DMSOs for GEO null distribution

--- project_scripts/GEO_query_signficiance_testing.py
+++ project_scripts/GEO_query_signficiance_testing.py
@@ -8,6 +8,7 @@ import pandas as pd
 import codecs
 import cmap.io.gct as gct
 import matplotlib.pyplot as plt
+from matplotlib.ticker import NullFormatter
 
 from pymongo import MongoClient
 from cmap.util.mongo_utils import CredentialsObject
@@ -208,8 +209,184 @@ def calc_ranks(cpF,oeF,shF,lmID):
     shPercent = shRank/float(len(shRank))
     return cpRank, cpPercent, oeRank, oePercent, shRank, shPercent
 
-### Obtain DMSOs 
+### Obtain DMSOs for permutation testing
+CM = mu.CMapMongo()
+cellList = ['A375','A549', 'HA1E', 'HCC515', 'HEPG2', 'HT29', 'MCF7', 'PC3', 'VCAP'] # cmap 'core' cell lines
+#grab only well combined dmsos:
+# dmsoQuery = CM.find({'pert_iname':'DMSO','cell_id':{'$in':cellList},'distil_nsample':{'$gte':2,'$lt':5}}, #, 
+#         {'sig_id':True,'pert_id':True,'cell_id':True,'pert_time':True,'is_gold':True,'pert_iname':True,'distil_ss':True,'distil_cc_q75':True,'brew_prefix':True,'up50_lm':True,'dn50_lm':True},
+#         toDataFrame=True)
+#grab all DMSOs:
+dmsoQuery = CM.find({'pert_iname':'DMSO','cell_id':{'$in':cellList},'distil_cc_q75':{'$lt':.6},'distil_ss':{'$lt':7},'distil_nsample':{'$lt':7}}, #, 
+        {'sig_id':True,'pert_id':True,'cell_id':True,'pert_time':True,'is_gold':True,'pert_iname':True,'distil_ss':True,'distil_cc_q75':True,'brew_prefix':True,'distil_nsample':True},
+        toDataFrame=True)
+# leave out signatures without distil_cc
+is_null = dmsoQuery['distil_cc_q75'] == -666
+dmsoQuery = dmsoQuery[~is_null.values]
+# remove anything on from the CNS plates
+is_cns = [x[:3] == 'CNS' for x in dmsoQuery['sig_id']]
+dmsoQuery = dmsoQuery[~np.array(is_cns)]
 
+# plateSet = set(dmsoQuery['brew_prefix'])
+ccQ75 = dmsoQuery['distil_cc_q75']
+SS = dmsoQuery['distil_ss']
+# write sig_ids to file
+outF = wkdir+'/dmso_sig_ids.grp'
+dmsoQuery['sig_id'].to_csv(outF,sep='\t',header=False,index=False)
+
+#plot relationship between CC and distil_nsample
+nsample = dmsoQuery['distil_nsample']
+plt.plot(nsample,ccQ75,'.')
+plt.xlabel('distil_nsample',fontweight='bold')
+plt.ylabel('CC_q75',fontweight='bold')
+plt.title('DMSO signatures - LINCS core cell lines (n = 7182)')
+outF = path.join(wkdir, 'LINCS_DMSOs_cc_by_distil_nsample.png')
+plt.savefig(outF, bbox_inches='tight',dpi=200)
+plt.close()
+
+
+
+# simple SC plot
+# ccQ75 = dmsoQuery['distil_cc_q75']
+# SS = dmsoQuery['distil_ss']
+# plt.plot(ccQ75,SS,'.')
+# plt.ylabel('ss',fontweight='bold')
+# plt.xlabel('CC_q75',fontweight='bold')
+# plt.title('DMSO signatures - LINCS core cell lines (n = 6471)')
+# outF = path.join(wkdir, 'LINCS_DMSOs_SC_plot.png')
+# plt.savefig(outF, bbox_inches='tight',dpi=200)
+# plt.close()
+
+### QQ plot with histogram
+x = ccQ75
+# y = SS
+y = nsample
+nullfmt   = NullFormatter()         # no labels
+# definitions for the axes
+left, width = 0.1, 0.65
+bottom, height = 0.1, 0.65
+bottom_h = left_h = left+width+0.02
+#
+rect_scatter = [left, bottom, width, height]
+rect_histx = [left, bottom_h, width, 0.2]
+rect_histy = [left_h, bottom, 0.2, height]
+# start with a rectangular Figure
+plt.figure(1, figsize=(8,8))
+axScatter = plt.axes(rect_scatter)
+axHistx = plt.axes(rect_histx)
+axHisty = plt.axes(rect_histy)
+# no labels
+axHistx.xaxis.set_major_formatter(nullfmt)
+axHisty.yaxis.set_major_formatter(nullfmt)
+# the scatter plot:
+axScatter.scatter(x, y) #,marker='.'
+# now determine nice limits by hand:
+binwidth = 0.25
+# xymax = np.max( [np.max(np.fabs(x)), np.max(np.fabs(y))] )
+xymax = np.max(np.fabs(y))
+# lim = ( int(xymax/binwidth) + 1) * binwidth
+lim = ( int(xymax/binwidth) + xymax) * binwidth
+#set 
+xMin = -.3
+xMax = 1
+xBinwidth = .05
+axScatter.set_xlim( (xMin, xMax) )
+axScatter.set_ylim( (0, lim) )
+#make hist
+binsX = np.arange(xMin, xMax + xBinwidth, xBinwidth)
+binsY = np.arange(0, lim + binwidth, binwidth)
+# axHistx.hist(x, bins=bins)
+axHistx.hist(x, 30, range=[xMin,xMax])
+axHisty.hist(y, bins=binsY, orientation='horizontal')
+#set lims
+# axHistx.set_xlim( axScatter.get_xlim() )
+# axHisty.set_ylim( axScatter.get_ylim() )
+# plt.show()
+# axScatter.set_ylabel('ss',fontweight='bold')
+axScatter.set_ylabel('distil_nsample',fontweight='bold')
+axScatter.set_xlabel('CC_q75',fontweight='bold')
+# axScatter.set_title('DMSO signatures - LINCS core cell lines (n = 6471)')
+# outF = path.join(wkdir, 'LINCS_DMSOs_SC_plot_hist.png')
+outF = path.join(wkdir, 'LINCS_DMSOs_cc_by_distil_nsample.png')
+plt.savefig(outF, bbox_inches='tight',dpi=200)
+plt.close()
+
+# make gmt for DMSO queries
+gmtListUp = []
+gmtListDn = []
+for i1 in dmsoQuery.index:
+    gmtDictUp = {}
+    gmtDictUp['id'] = dmsoQuery.ix[i1,'sig_id']
+    gmtDictUp['desc'] = dmsoQuery.ix[i1,'sig_id']
+    gmtDictUp['sig'] = dmsoQuery.ix[i1,'up50_lm']
+    gmtListUp.append(gmtDictUp)
+    gmtDictDn = {}
+    gmtDictDn['id'] = dmsoQuery.ix[i1,'sig_id']
+    gmtDictDn['desc'] = dmsoQuery.ix[i1,'sig_id']
+    gmtDictDn['sig'] = dmsoQuery.ix[i1,'dn50_lm']
+    gmtListDn.append(gmtDictDn)
+gmtOutUp = wkdir + '/DMSO_lincs_core_lines_Up_lm50.gmt'
+gmtOutDn = wkdir + '/DMSO_lincs_core_lines_Dn_lm50.gmt'
+gmt.write(gmtListUp,gmtOutUp)
+gmt.write(gmtListDn,gmtOutDn)
+
+
+larger_query_matrix = '/xchip/cogs/projects/connectivity/query/wtcs.lm50/sim_wtcs.lm50_COMBINED.gctx'
+gt = gct.GCT()
+gt.read(larger_query_matrix,)
+
+
+#once the gmt is made:
+# 1) run sig_query_tool
+# 2) run sig_summly_tool
+# 3) 
+
+
+
+# metric = 'wtcs'
+# queryDir = work_dir + '/ctd2_sig_query'
+# if not os.path.exists(queryDir):
+#     os.mkdir(queryDir)
+# cmd = ' '.join(['rum -q local -f sig_query_tool',
+#          '--sig_id ' + file1,
+#          '--metric ' + metric,
+#          '--column_space full',
+#          '--out ' + queryDir,
+#          '--mkdir false',
+#          '--save_tail false'])
+#          # '--row_space bing', 
+# os.system(cmd)
+
+# for cellLine in cellList:
+#     # cellLine = 'A375'
+#     wkdir = '/xchip/cogs/projects/NMF/BRAF_PCL/' + cellLine
+#     if not os.path.exists(wkdir):
+#         os.mkdir(wkdir)
+#     # get signature annotations from cmap database
+#     CM = mu.CMapMongo()
+#     goldQuery = CM.find({'is_gold' : True,'pert_id':{'$in':brdAllGroups},'cell_id':cellLine,'pert_dose':{'$gt':1}}, #, 
+#             {'sig_id':True,'pert_id':True,'cell_id':True,'pert_time':True,'is_gold':True,'pert_iname':True,'distil_ss':True,'distil_cc_q75':True},
+#             toDataFrame=True)
+#     indRep = [x.replace(":",".") for x in goldQuery['sig_id']]
+#     indRep = [x.replace("-",".") for x in indRep]
+#     goldQuery.index = indRep
+#     # goldQuery.index = goldQuery['sig_id']
+#     dmsoQuery = CM.find({'pert_iname':'DMSO','cell_id':cellLine,'distil_nsample':{'$gte':2,'$lt':5}}, #, 
+#             {'sig_id':True,'pert_id':True,'cell_id':True,'pert_time':True,'is_gold':True,'pert_iname':True,'distil_ss':True,'distil_cc_q75':True},
+#             toDataFrame=True)
+#     indRep = [x.replace(":",".") for x in dmsoQuery['sig_id']]
+#     indRep = [x.replace("-",".") for x in indRep]
+
+
+
+# random dos cp lookup
+# CM = mu.CMapMongo()
+# cellList = ['A375','A549', 'HA1E', 'HCC515', 'HEPG2', 'HT29', 'MCF7', 'PC3', 'VCAP'] # cmap 'core' cell lines
+# dosQuery = CM.find({'pert_id':'BRD-K77790153'}, #, 
+#         {'sig_id':True,'pert_id':True,'cell_id':True,'pert_time':True,'is_gold':True,'pert_iname':True,'distil_ss':True,'distil_cc_q75':True,'brew_prefix':True,'pert_dose':True},
+#         toDataFrame=True)
+# outF = wkdir+'/BRD-K77790153_signature_info.txt'
+# dosQuery.to_csv(outF,sep='\t',header=True,index=False)
 
 
 
