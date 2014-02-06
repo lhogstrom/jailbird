96e9166e945c9721a90b10cbb2148d803cde1f13
Larson Hogstrom <hogstrom@broadinstitute.org>
2014-02-06 15:25:25 -0500

    kinase update

--- drug_class/kinase_clustering.py
+++ drug_class/kinase_clustering.py
@@ -8,10 +8,13 @@ LH 02/2014
 import os
 import numpy as np, pandas as pd, scipy as sp
 from matplotlib import pyplot as plt
+from pymongo import MongoClient
 from cmap.analytics.statsig import ConnectivitySignificance
 from cmap.io import gct
 from cmap.util.mongo_utils import CredentialsObject
-from pymongo import MongoClient
+from matplotlib import cm
+import matplotlib.gridspec as gridspec
+
 
 wkdir = '/xchip/cogs/projects/pharm_class/lhwork/kinase_clustering'
 if not os.path.exists(wkdir):
@@ -42,7 +45,6 @@ summIn.index = pInames
 summIn.columns = pInames
 summClust = summIn.reindex(index=inamesCluster,columns=inamesCluster)
 
-
 # load in expected molecular targets of each drug
 #import 
 aFile = '/xchip/cogs/projects/pharm_class/lhwork/kinase_clustering/drug_annotations.txt'
@@ -50,15 +52,36 @@ annFrm = pd.read_csv(aFile,sep='\t')
 annFrm = annFrm.reindex(columns=['sum_id','pert_iname','targets'])
 annFrm.index = annFrm['pert_iname']
 targetFrm = annFrm[annFrm.pert_iname.isin(pInames) & annFrm.sum_id.isin(pIDs)]
+targetFrm.targets = targetFrm.targets.str.split(', ')
 targetFrm = targetFrm.reindex(summClust.index)
 out = wkdir + '/kinase_targets_clustered.txt'
 targetFrm.to_csv(out,sep='\t',headers=True)
-plt.close()
 
-# LY-294002
+# order targets by freq
+# loop through each target
+tarLst = [item for sublist in targetFrm.targets for item in sublist]
+tarSer = pd.Series(tarLst)
+# targetSet = set(tarLst)
+tarCounts = tarSer.value_counts()
+targetMatch = targetFrm.copy()
+nTarget = targetMatch.shape[0]
+boolFrm = pd.DataFrame(np.zeros([nTarget,len(tarCounts)]),index=targetFrm.index,columns=tarCounts.index)
+for tar in tarCounts.index:
+    def check_cell_contents(x):
+        if tar in x:
+            return 1
+        else:
+            return 0
+    boolMatch = targetFrm.targets.apply(check_cell_contents)
+    targetMatch[tar] = boolMatch
+    boolFrm.ix[:,tar] = boolMatch
+out = wkdir + '/kinase_targets_match.txt'
+targetMatch.to_csv(out,sep='\t',headers=True)
+
 #make heatmap of clustered data
 tmpClust = summClust.ix[:30,:30]
 fig = plt.figure(1, figsize=(10, 10))
+plt.subplot(1,2,1)
 plt.imshow(tmpClust.values,
     interpolation='nearest',
     aspect='auto')
@@ -73,25 +96,91 @@ plt.colorbar()
 # plt.xlabel(matrixType + ' threshold')
 # plt.ylabel('unique perturbations')
 # plt.title('summly false positive rate - based on DMSO')
+# target status
+plt.subplot(1,2,2)
+smFrm = pd.DataFrame(boolFrm['MAP2K1'][:30])
+plt.imshow(smFrm,
+    interpolation='nearest',
+    aspect='auto')
+#save
 out = wkdir + '/kinase_summly_clustering.png'
 plt.savefig(out, bbox_inches='tight')
 plt.close()
 
-# order targets by freq
-# loop through each target
-# make graph of clustering for each target
+### target status heatmap
+#make heatmap of clustered data
+nStart = 55
+nFinish = 90
+tmpClust = summClust.ix[nStart:nFinish,nStart:nFinish]
+fig = plt.figure(1, figsize=(10, 10))
+gs = gridspec.GridSpec(1, 2, width_ratios=[40, 1]) 
+ax0 = plt.subplot(gs[0])
+# plt.subplot(1,2,1)
+ax0.imshow(tmpClust.values,
+    interpolation='nearest',
+    aspect='auto',
+    vmin=-100, 
+    vmax=100)
+tickRange = range(0,tmpClust.shape[0])
+xtcks = [x for x in tmpClust.index]
+plt.xticks(tickRange, xtcks,rotation=90)
+plt.yticks(np.arange(len(xtcks)),xtcks)
+# plt.colorbar()
+# plt.xlabel(matrixType + ' threshold')
+# plt.ylabel('unique perturbations')
+plt.title('Kinase clustering')
+# target status
+target = 'SYK'
+smFrm = pd.DataFrame(boolFrm[target][nStart:nFinish])
+ax1 = plt.subplot(gs[1])
+ax1.imshow(smFrm,
+    interpolation='nearest',
+    aspect='auto')
+ax1.axes.get_yaxis().set_visible(False)
+plt.xticks([0], ['labeled target - ' + target],rotation=90)
+out = wkdir + '/' + target + '_target_label_clustering.png'
+plt.savefig(out, bbox_inches='tight')
+plt.close()
+
+### rolling sum window
+for tar in tarCounts.index[:70]:
+    target = tar
+    window=10
+    rollSum = pd.stats.moments.rolling_sum(boolFrm[target],window)
+    out = wkdir + '/' + target + '_rolling_sum.png'
+    plt.plot(rollSum)
+    plt.ylim((0,window))
+    plt.xlabel('cluster axis')
+    plt.ylabel('rolling sum')   
+    plt.title(target + ' - target density from clustering - window = ' +str(window)) 
+    plt.savefig(out, bbox_inches='tight')
+    plt.close()
 
 
+# heatmap of targets
+tmpBool = boolFrm.ix[:,:30]
+fig = plt.figure(1, figsize=(10, 50))
+plt.imshow(tmpBool.values,
+    interpolation='nearest',
+    aspect='auto',
+    cmap=cm.Greys)
+    # vmin=0, 
+    # vmax=1,
+ytickRange = range(0,tmpBool.shape[0])
+xtickRange = range(0,tmpBool.shape[1])
+ytcks = [x for x in tmpBool.index]
+xtcks = [x for x in tmpBool.columns]
+# ax.xaxis.set_ticks_position('top')
+plt.xticks(xtickRange, xtcks,rotation=90)
+plt.yticks(ytickRange,ytcks)
+# plt.colorbar()
+# plt.xlabel(matrixType + ' threshold')
+# plt.ylabel('unique perturbations')
+# plt.title('summly false positive rate - based on DMSO')
+out = wkdir + '/kinase_bool_targets.png'
+plt.savefig(out, bbox_inches='tight')
+plt.close()
 
-
-
-
-
-
-
-
-
-
-
-
+# loop through each cell line - apply summly dendrogram to order to all
+# make heatmap
 
